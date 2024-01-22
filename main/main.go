58bdd989e27528acd48ad01784675158648d1b49
Sébastien Hauri <github.gottfredson@simplelogin.com>
2024-01-22 10:40:27 +0100

    feat: Measure the user and issuer time separately

--- main/main.go
+++ main/main.go
@@ -38,26 +38,49 @@ func main() {
 	var t []byte
 	var z_star, query, resp []uint8
 	println("Benchmarking interactive issuance..")
+
+	var user0_time time.Duration = 0
+	var issuer_time time.Duration = 0
+	var user1_time time.Duration = 0
 	start = time.Now()
 	for i := 0; i < constants.MEASURE_ROUNDS; i++ {
+		// User0
+		start_user0 := time.Now()
 		t, z_star, query = mqat.User0(mqat_pk)
+		end_user0 := time.Since(start_user0)
+		user0_time += end_user0
 		if query == nil {
 			println("\t Query is nil at iteration", i)
 			continue
 		}
+
+		// Sign0
+		start_issuer := time.Now()
 		resp = mqat.Sign0(mqat_sk, query)
+		end_issuer := time.Since(start_issuer)
+		issuer_time += end_issuer
 		if resp == nil {
 			println("\t Response is nil at iteration", i)
 			continue
 		}
+
+		// User1
+		start_user1 := time.Now()
 		token = mqat.User1(mqat_pk, t, z_star, resp)
+		end_user1 := time.Since(start_user1)
+		user1_time += end_user1
 		if token == nil {
 			println("\t Token is nil at iteration", i)
 		}
 	}
 	end = time.Since(start)
+	user_time := user0_time + user1_time
 	fmt.Printf("\t Time elapsed: %.3f seconds.\n", end.Seconds())
-	fmt.Printf("\t Mean time per signature %.3f milliseconds.\n\n", float64(end.Milliseconds())/constants.MEASURE_ROUNDS)
+	fmt.Printf("\t Mean time per issuance %.3f milliseconds.\n", float64(end.Milliseconds())/constants.MEASURE_ROUNDS)
+	fmt.Printf("\t\t Total time User: %.3f seconds.\n", user_time.Seconds())
+	fmt.Printf("\t\t Mean time User: %.3f milliseconds.\n", float64(user_time.Milliseconds())/constants.MEASURE_ROUNDS)
+	fmt.Printf("\t\t Total time Issuer: %.3f seconds.\n", issuer_time.Seconds())
+	fmt.Printf("\t\t Mean time per Issuer %.3f milliseconds.\n\n", float64(issuer_time.Milliseconds())/constants.MEASURE_ROUNDS)
 
 	println("Benchmarking verification..")
 	start = time.Now()
@@ -69,100 +92,7 @@ func main() {
 	}
 	end = time.Since(start)
 	fmt.Printf("\t Time elapsed: %.3f seconds.\n", end.Seconds())
-	fmt.Printf("\t Mean time per verification %.3f milliseconds.\n\n", float64(end.Milliseconds())/constants.MEASURE_ROUNDS)
+	fmt.Printf("\t Mean time per verification: %.3f milliseconds.\n", float64(end.Milliseconds())/constants.MEASURE_ROUNDS)
 
 	println("===== MQAT =====")
-
-	// println()
-
-	// println("===== UOV =====")
-	// uov := crypto.NewUOV(constants.M, constants.N, constants.UOV_PK_SEED_LEN, constants.UOV_SK_SEED_LEN)
-	// fmt.Printf("Initialising an UOV with m=%d and n=%d ..\n", uov.M, uov.N)
-	// var uov_sk *crypto.UOVSecretKey
-	// var uov_pk *crypto.UOVPublicKey
-	// println("Benchmarking key generation..")
-	// start := time.Now()
-	// for i := 0; i < constants.MEASURE_ROUNDS; i++ {
-	// 	uov_sk, uov_pk = uov.KeyGen()
-	// 	if uov_pk == nil || uov_sk == nil {
-	// 		println("\t New key pair is nil at iteration", i)
-	// 	}
-	// }
-	// end := time.Since(start)
-	// fmt.Printf("\t Time elapsed: %d milliseconds.\n", end.Milliseconds())
-	// fmt.Printf("\t Mean time per key generation: %.3f milliseconds.\n\n", float64(end.Milliseconds())/constants.MEASURE_ROUNDS)
-
-	// println("Benchmarking signature..")
-	// var sig []uint8
-	// message := crypto.Nrand128(constants.M, []byte{0})
-	// start = time.Now()
-	// for i := 0; i < constants.MEASURE_ROUNDS; i++ {
-	// 	sig = uov.Sign(message, uov_sk)
-	// 	if sig == nil {
-	// 		println("\t Signature is nil at iteration", i)
-	// 	}
-	// }
-	// end = time.Since(start)
-	// fmt.Printf("\t Time elapsed: %.3f seconds.\n", end.Seconds())
-	// fmt.Printf("\t Mean time per signature %.3f milliseconds.\n\n", float64(end.Milliseconds())/constants.MEASURE_ROUNDS)
-
-	// println("Benchmarking verification..")
-	// start = time.Now()
-	// for i := 0; i < constants.MEASURE_ROUNDS; i++ {
-	// 	bool := uov.Verify(message, sig, uov_pk)
-	// 	if !bool {
-	// 		println("\t Verification failed at iteration", i)
-	// 	}
-	// }
-	// end = time.Since(start)
-	// fmt.Printf("\t Time elapsed: %.3f seconds.\n", end.Seconds())
-	// fmt.Printf("\t Mean time per verification %.3f milliseconds.\n", float64(end.Milliseconds())/constants.MEASURE_ROUNDS)
-	// println("===== UOV =====")
-
-	// println()
-
-	// println("===== MQDSS =====")
-	// mqdss := crypto.NewMQDSS(uov.M, uov.N+uov.M, constants.MQDSS_ROUNDS, constants.MQDSS_PK_SEED_LEN, constants.MQDSS_SK_SEED_LEN)
-	// fmt.Printf("Initialising an MQDSS with m=%d, n=%d, r=%d ..\n", mqdss.M, mqdss.N, mqdss.R)
-	// var mqdss_sk *crypto.MQDSSSecretKey
-	// var mqdss_pk *crypto.MQDSSPublicKey
-	// println("Benchmarking key generation..")
-	// start = time.Now()
-	// for i := 0; i < constants.MEASURE_ROUNDS; i++ {
-	// 	mqdss_sk, mqdss_pk = mqdss.KeyGen()
-	// 	if mqdss_sk == nil || mqdss_pk == nil {
-	// 		println("\t New key pair is nil at iteration", i)
-	// 	}
-	// }
-	// end = time.Since(start)
-	// fmt.Printf("\t Time elapsed: %d milliseconds.\n", end.Milliseconds())
-	// fmt.Printf("\t Mean time per key generation: %.3f milliseconds.\n\n", float64(end.Milliseconds())/constants.MEASURE_ROUNDS)
-
-	// println("Benchmarking signature..")
-	// sig = make([]byte, 2*constants.HASH_BYTES+int(mqdss.R)*(constants.M+2*constants.N*constants.HASH_BYTES))
-	// start = time.Now()
-	// for i := 0; i < constants.MEASURE_ROUNDS; i++ {
-	// 	sig = mqdss.Sign([]byte("Hey, this is my message to sign..."), mqdss_sk)
-	// 	if sig == nil {
-	// 		println("\t Signature is nil at iteration", i)
-	// 	}
-	// }
-	// end = time.Since(start)
-	// fmt.Printf("\t Time elapsed: %.3f seconds.\n", end.Seconds())
-	// fmt.Printf("\t Mean time per signature %.3f milliseconds.\n\n", float64(end.Milliseconds())/constants.MEASURE_ROUNDS)
-
-	// println("Benchmarking verification..")
-	// start = time.Now()
-	// for i := 0; i < constants.MEASURE_ROUNDS; i++ {
-	// 	bool := mqdss.Verify([]byte("Hey, this is my message to sign..."), sig, mqdss_pk)
-	// 	if !bool {
-	// 		println("\t Verification failed at iteration", i)
-	// 	}
-	// }
-	// end = time.Since(start)
-	// fmt.Printf("\t Time elapsed: %.3f seconds.\n", end.Seconds())
-	// fmt.Printf("\t Mean time per verification %.3f milliseconds.\n\n", float64(end.Milliseconds())/constants.MEASURE_ROUNDS)
-
-	// println("Signature size:", len(sig), "bytes.")
-	// println("===== MQDSS =====")
 }
